427614ef5862fcf71583c250f6f8d9c09235ac78
Suraj Thapa <stsurajst@gmail.com>
2020-11-21 12:08:36 +0545

    expo location added

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const WEATHER_API_KEY = 'b062803a275ee4bc06537e95f676ab34';
+
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+
 export default function App() {
+  const [error, setError] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitSystem, setUnitSystem] = useState('metric');
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function load() {}
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
