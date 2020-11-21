e56510c43370a3d3d41071aaa50a7daddec24141
Suraj Thapa <stsurajst@gmail.com>
2020-11-21 23:31:54 +0545

    expo location updated

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Location from 'expo-location';
+
 const WEATHER_API_KEY = 'b062803a275ee4bc06537e95f676ab34';
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 
 export default function App() {
-  const [error, setError] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
   const [currentWeather, setCurrentWeather] = useState(null);
   const [unitSystem, setUnitSystem] = useState('metric');
 
@@ -15,7 +17,15 @@ export default function App() {
     load();
   }, []);
 
-  async function load() {}
+  async function load() {
+    try {
+      const { status } = async Location.requestPermissionsAsync();
+      if(status !== 'granted'){
+        setErrorMessage("Access to location is needed to run the app");
+        return;
+      }
+    } catch (error) {}
+  }
 
   return (
     <View style={styles.container}>
